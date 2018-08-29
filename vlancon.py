76a44cac86bdab3e8be500ca600023729d5f77fa
Daniel Solstad <solstad.daniel@gmail.com>
2018-08-29 21:59:07 +0200

    Update vlancon.py

--- vlancon.py
+++ vlancon.py
@@ -1,9 +1,13 @@
-#!/bin/python3
+#!/usr/bin/env python3
 ##
 ## This is nothing else than a wrapper around nmap.
 ## Author: Daniel Solstad (dsolstad.com)
 ##
 
+
+#sudo ip addr add 192.168.1.2/24 dev eth0;
+#sudo ip route add default via 192.168.1.1 dev eth0;
+#sudo ip link set dev eth0 up/down
 import sys
 
 if not sys.version_info[0] == 3:
@@ -17,42 +21,60 @@ import subprocess as sub
 import ipaddress
 from termcolor import colored
 
-def vlan_add(network, netmask, vlan, interface):
+def vlan_add(interface, vlan):
     subinterface = interface + "." + vlan
-    sub.call(['ifconfig', interface, 'down'], stdout=sub.PIPE, stderr=sub.PIPE)
-    sub.call(['ifconfig', subinterface, 'down'], stdout=sub.PIPE, stderr=sub.PIPE)
-    sub.call(['vconfig', 'rem', subinterface], stdout=sub.PIPE, stderr=sub.PIPE)
     sub.call(['vconfig', 'add', interface, vlan], stdout=sub.PIPE, stderr=sub.PIPE)
-    sub.call(['ifconfig', interface, 'up'], stdout=sub.PIPE, stderr=sub.PIPE)
-    
+    sub.call(['ip', 'link', 'set', 'dev', interface, 'up'], stdout=sub.PIPE, stderr=sub.PIPE)
+    sub.call(['ip', 'link', 'set', 'dev', subinterface, 'up'], stdout=sub.PIPE, stderr=sub.PIPE)
+
 def vlan_rem(interface, vlan):
     subinterface = interface + "." + vlan
-    sub.call(['ifconfig', subinterface, 'down'], stdout=sub.PIPE, stderr=sub.PIPE)
+    sub.call(['ip', 'link', 'set', 'dev', interface, 'down'], stdout=sub.PIPE, stderr=sub.PIPE)
+    sub.call(['ip', 'link', 'set', 'dev', subinterface, 'up'], stdout=sub.PIPE, stderr=sub.PIPE)
     sub.call(['vconfig', 'rem', subinterface], stdout=sub.PIPE, stderr=sub.PIPE)
 
-def set_ip_addr(interface, vlan, ip_addr, netmask):
+def set_ip_addr(interface, vlan, ipaddr):
     subinterface = interface + "." + vlan
-    sub.call(['ifconfig', subinterface, ip_addr, 'netmask', netmask, 'up'], stdout=sub.PIPE, stderr=sub.PIPE)
+    sub.call(['ip', 'addr', 'add', ipaddr, 'dev', subinterface], stdout=sub.PIPE, stderr=sub.PIPE)
 
-def gateway_add(gateway):
-    sub.check_output(['route', 'add', 'default', 'gw', gateway])
+def gateway_add(interface, vlan, gateway):
+    subinterface = interface + "." + vlan    
+    sub.call(['ip', 'route', 'add', 'default', 'via', gateway, 'dev', subinterface], stdout=sub.PIPE, stderr=sub.PIPE)
 
 def gateway_rem(gateway):
-    sub.check_output(['route', 'del', 'default', 'gw', gateway])
+    sub.call(['ip', 'route', 'del', 'default', 'via', gateway, 'dev', subinterface], stdout=sub.PIPE, stderr=sub.PIPE)
 
 # Checks for an available IP-address. Trying to get the highest available.
-def get_ip_addr(interface, vlan, network, netmask):
-    subinterface = interface + "." + vlan
-    cmd = ['arp-scan', '--interface=' + subinterface, network + '/' + str(mask2bits(netmask))]
+def get_ip_addr(interface, vlan, network):
+    cmd = ['arp-scan', '--interface=' + subinterface, network]
+    #cmd = ['arp', '-a', '-i', interface + "." + vlan]
     p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
     res, err = p.communicate()
     # Filter out all valid IP addresses
     ips = re.findall(b"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", res)
     # Loop through and return the highest available IP address
     # Debug: print(get_ip_range(network, netmask))
-    for addr in reversed(get_ip_range(network, netmask)):
+    cidr = network.split('/')
+    print (cidr)
+    for addr in reversed(get_ip_range(network)):
         if addr not in ips:
-            return addr
+            return addr + cidr[1]
+    return False
+
+# Kick start the arp table population before checking for an available IP addr.
+# Need to use --send-ip in nmap to send the requests through OS sockets and 
+#  not directly on the network or else the arp table wont be filled up.
+def populate_arp(interface, vlan, network):
+    subinterface = interface + '.' + vlan
+    #sub.call(['nmap', '-sn', network, '-e', subinterface, '--send-ip'])
+    for i in range(1, 100):
+        try:
+            res = sub.check_output(['arp', '-a', '-i', subinterface])
+            # If there no incomplete entries we return
+            if res.find('incomplete') == -1:
+                return True
+        except:
+            time.sleep(1)
     return False
 
 # Checking if the gateway is responsive. Waiting 60 seconds.
@@ -67,8 +89,8 @@ def check_gateway(gateway):
     return False
 
 # Gets the last octet of all valid IP addresses in the network range
-def get_ip_range(network, netmask):
-    net = ipaddress.ip_network(network + "/" + netmask)
+def get_ip_range(network):
+    net = ipaddress.ip_network(network)
     ip_range = []
     for addr in list(map(str, net.hosts())):
         ip_range.append(addr)
@@ -82,11 +104,11 @@ def mask2bits(netmask):
 if __name__ == "__main__":
 
     help = """
-    vlancon.py add <network> <netmask> <vlan nr> <interface> <gateway>
+    vlancon.py add <network/<cidr>> <vlan nr> <interface> <gateway>
     vlancon.py rem <interface> <vlan nr> <gateway>
 
     Example:
-    vlancon.py add 192.168.1.0 255.255.255.0 101 eth1 192.168.1.1
+    vlancon.py add 192.168.1.0/24 101 eth1 192.168.1.1
     vlancon.py rem eth1 101 192.168.1.1
     """    
 
@@ -111,26 +133,30 @@ if __name__ == "__main__":
         print (colored('Done.', 'green'))
 
     elif sys.argv[1] == 'add':
-        if len(sys.argv) != 7:
+        if len(sys.argv) != 6:
             print (colored('Missing one or more arguments', 'red'))
             print (help)
             sys.exit()
 
-        network = sys.argv[2]
-        netmask = sys.argv[3]
-        vlan = sys.argv[4]
-        interface = sys.argv[5]
-        gateway = sys.argv[6]
+        network = sys.argv[2]       # e.g 192.168.1.0/24
+        vlan = sys.argv[3]          # e.g 101
+        interface = sys.argv[4]     # e.g eth1
+        gateway = sys.argv[5]       # e.g 192.168.1.1
         
         #print ("[+] ")
         print ('[+] Adding interface ' + interface + '.' + vlan + ' (' + network + ')')
-        vlan_add(network, netmask, vlan, interface)
+        vlan_rem(interface, vlan)
+        vlan_add(interface, vlan)
         print (colored('[+] Interface added.', 'green'))
+        
+        print ('[+] Waiting for ARP table to update.')
+        populate_arp(interface, vlan, network)
+
         print ('[+] Checking for an available IP-address')
-        ip_addr = get_ip_addr(interface, vlan, network, netmask)
-        if ip_addr != False:
-            set_ip_addr(interface, vlan, ip_addr, netmask)
-            print (colored('[+] Successfully set IP-address: ' + str(ip_addr), 'green'))
+        ipaddr = get_ip_addr(interface, vlan, network)
+        if ipaddr != False:
+            set_ip_addr(interface, vlan, ipaddr)
+            print (colored('[+] Successfully set IP-address: ' + str(ipaddr), 'green'))
         else:
             print (colored('[+] Error - Could not find any available IP addresses. Aborting.', 'red'))
             sys.exit()
@@ -139,6 +165,7 @@ if __name__ == "__main__":
         gateway_add(gateway)
         print (colored('[+] Gateway added.', 'green'))
         print ("[+] Checking if gateway is responding")
+
         if check_gateway(gateway):
             print (colored('[+] Success. Gateway responding.', 'green'))
         else:
